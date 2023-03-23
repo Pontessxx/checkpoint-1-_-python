8d9d1bb86265372ecd8e32bb164bcb010e2f8471
Pontes <hpo.pontes@gmail.com>
2023-03-23 19:32:27 -0300

    implementando a funcao replace seven

--- checkpoint.py
+++ checkpoint.py
@@ -1,3 +1,15 @@
+def replace_sevens(n):
+    """Substitui todos os dígitos 7 por 0 em um número."""
+    result = 0
+    multiplier = 1
+    while n > 0:
+        digit = n % 10
+        if digit == 7:
+            digit = 0
+        result += digit * multiplier
+        multiplier *= 10
+        n //= 10
+    return result
 print("******************************")
 print("***checkpoint 1 with python***")
 print("******************************")
@@ -13,16 +25,11 @@ while True:
         print("\n Os numeros devem estar entre 0 a 99")
         print("---Tente novamente!---")
     else:
-        #subistituir numeros 7 em 0
-        n1 = int(str(n1).replace('7', '0'))
-        n2 = int(str(n2).replace('7', '0'))
 
-        soma_teste = n1 + n2
-        if '7' in str(soma_teste):
-            soma_teste = int(str(soma_teste).replace('7','0'))
+        n1 = replace_sevens
+        
 
         print(f"Soma dos numeros: {n1} + {n2} = {soma_teste}")
         break
-
-#teste numero 2 
+    
 # não devo usar string tentar novaemnte
